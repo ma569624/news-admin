5e65478bebb887af5956d19a5783efc7f745c7c0
manish <ms569624@gmail.com>
2024-04-20 22:25:39 +0530

    add create date

--- src/ApiCalls/ApiCalls.js
+++ src/ApiCalls/ApiCalls.js
@@ -3,7 +3,7 @@ import { ApiContext } from "../Context/ApiContext";
 
 const ApiCalls = async (params, method = 'GET', value) => {
 //   const API = useContext(ApiContext);
-const API = 'http://localhost:5000'
+const API = 'https://api.techdeveloper.in'
 
     try {
         const resdata = await fetch(`${API}/api/${params}`, {
